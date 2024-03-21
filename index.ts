d66c7538b708b1aaad9682ae3820f7325e936839
zohaib617 <bikakparvez@gmail.com>
2024-03-21 12:02:11 -0700

    a simple line calculator

---
+++ index.ts
@@ -0,0 +1,25 @@
+#! /usr/bin/env node
+import inquirer from "inquirer";
+
+const answer = await inquirer.prompt([
+    { message: "Enter first number", type:"number", name: "firstNumber"},
+    { message: "Enter second number", type:"number", name: "secondNumber"},
+    {message: "Select one of the oprators to perform operation",
+    type:"list",
+    name:"operator",
+    choices:["Addition", "Subtraction","Multipilaction","Division"],
+}
+]);
+
+//Condition Statment
+if (answer.operator === "Addition") {
+    console.log(answer.firstNumber + answer.secondNumber);
+}else if(answer.operator === "Subtraction") {
+    console.log(answer.firstNumber - answer.secondNumber);
+ } else if (answer.operator === "Multipilaction") {
+    console.log(answer.firstNumber * answer.secondNumber);
+ } else if (answer.operator === "Division") {
+    console.log(answer.firstNumber /  answer.secondNumber);
+ } else {
+    console.log("Please select valid operator")
+ }
